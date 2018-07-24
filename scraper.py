7877cd2520db5e63c3343164d1cb264d18d0021c
blablupcom <o.volodin@yahoo.com>
2018-07-24 19:34:14 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,7 +10,8 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 
 
-#### FUNCTIONS 1.0
+#### FUNCTIONS 1.2
+import requests
 
 def validateFilename(filename):
     filenameregex = '^[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[0-9][0-9][0-9][0-9]_[0-9QY][0-9]$'
@@ -38,19 +39,19 @@ def validateFilename(filename):
 
 def validateURL(url):
     try:
-        r = urllib2.urlopen(url)
+        r = requests.get(url)
         count = 1
-        while r.getcode() == 500 and count < 4:
+        while r.status_code == 500 and count < 4:
             print ("Attempt {0} - Status code: {1}. Retrying.".format(count, r.status_code))
             count += 1
-            r = urllib2.urlopen(url)
-        sourceFilename = r.geturl()
+            r = requests.get(url)
+        sourceFilename = r.headers.get('Content-Disposition')
         if sourceFilename:
             ext = os.path.splitext(sourceFilename)[1].replace('"', '').replace(';', '').replace(' ', '')
         else:
             ext = os.path.splitext(url)[1]
-        validURL = r.getcode() == 200
-        validFiletype = ext.lower() in ['.csv', '.xls', '.xlsx']
+        validURL = r.status_code == 200
+        validFiletype = ext.lower() in ['.csv', '.xls', '.xlsx', '.pdf']
         return validURL, validFiletype
     except:
         print ("Error validating URL.")
@@ -84,34 +85,75 @@ def convert_mth_strings ( mth_string ):
 
 #### VARIABLES 1.0
 
-entity_id = "E1734_FBC_gov"
-url = "http://www.fareham.gov.uk/about_the_council/financial_information/expenditurover500.aspx"
+entity_id = "E1537_EFDC_gov"
+url = "http://www.eppingforestdc.gov.uk/index.php/home/file-store/category/6-csv?start=20"
 errors = 0
 data = []
+d = {'limit': '0', '0f9358dcf0d8b9de65b32d49f9a7e8f7': '1'}
 
 
 #### READ HTML 1.0
 
-html = urllib2.urlopen(url)
-soup = BeautifulSoup(html, 'lxml')
+html = requests.post(url, data=d)
+soup = BeautifulSoup(html.text, 'lxml')
 
 #### SCRAPE DATA
 
-ul_blocks = soup.find_all('div', attrs = {'class': 'csstable'})
-for ul_block in ul_blocks:
-    links = ul_block.find_all('a')
-    for link in links:
-        url = link['href']
-        if 'http' not in url:
-            url = 'http://www.fareham.gov.uk' + url
-        else:
-            url = url
-        if '.csv' in url:
-            file_name = link.text.strip()
-            csvMth = file_name[:3]
-            csvYr = file_name.split()[1]
-            csvMth = convert_mth_strings(csvMth.upper())
-            data.append([csvYr, csvMth, url])
+blocks = soup.find_all('div', "pd-filenamebox")
+for block in blocks:
+    if 'http' not in block.find('a')['href']:
+        url = 'http://www.eppingforestdc.gov.uk' + block.find('a')['href']
+    else:
+        url = block.find('a')['href']
+    file_name = block.find('a').text.strip()
+    csvMth = csvYr = ''
+    if 'Q4' in file_name:
+        csvMth = 'Q4'
+        year_text = re.match(r'.*([1-3][0-9]{3})', file_name)
+        if year_text is not None:
+            csvYr =year_text.group(1)
+    elif 'Q3' in file_name:
+        csvMth = 'Q3'
+        year_text = re.match(r'.*([1-3][0-9]{3})', file_name)
+        if year_text is not None:
+            csvYr = year_text.group(1)
+    elif 'Q2' in file_name:
+        csvMth = 'Q2'
+        year_text = re.match(r'.*([1-3][0-9]{3})', file_name)
+        if year_text is not None:
+            csvYr = year_text.group(1)
+    elif 'Q1' in file_name:
+        csvMth = 'Q1'
+        year_text = re.match(r'.*([1-3][0-9]{3})', file_name)
+        if year_text is not None:
+            csvYr = year_text.group(1)
+    elif 'April 2010 to December 2010' in file_name:
+        csvMth = 'Q0'
+        csvYr = '2010'
+    elif 'q4 2015-16' in file_name:
+        csvMth = 'Q1'
+        csvYr = '2016'
+    elif 'q1' in file_name:
+        csvMth = 'Q2'
+        year_text = re.match(r'.*([1-3][0-9]{3})', file_name)
+        if year_text is not None:
+            csvYr = year_text.group(1)
+    elif 'q2' in file_name:
+        csvMth = 'Q3'
+        year_text = re.match(r'.*([1-3][0-9]{3})', file_name)
+        if year_text is not None:
+            csvYr = year_text.group(1)
+    elif 'q3' in file_name:
+        csvMth = 'Q4'
+        year_text = re.match(r'.*([1-3][0-9]{3})', file_name)
+        if year_text is not None:
+            csvYr = year_text.group(1)
+    else:
+        csvMth = file_name.split()[-2][:3]
+        csvYr = file_name.split()[-1]
+    csvMth = convert_mth_strings(csvMth.upper())
+    data.append([csvYr, csvMth, url])
+
 
 #### STORE DATA 1.0
 
